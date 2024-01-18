f2d4ff5565acb3678ae9530059a1fdbbc39ea80e
Rahul Maharjan <rahulmaharjan252@gmail.com>
2024-01-18 20:05:32 +0545

    Nav-bar improved CSS

--- src/components/Nav.js
+++ src/components/Nav.js
@@ -1,93 +1,62 @@
 import React from "react";
 import { Link, useNavigate } from "react-router-dom";
 import { isAuthenticated, signout } from "../pages/auth";
+import "./Nav.css";
 
 const Nav = () => {
   const navigate = useNavigate();
   return (
     <>
-      <nav className="navbar navbar-expand-lg bg-body-tertiary">
-        <div className="container-fluid">
-          <Link className="navbar-brand" to="#">
-            Navbar
+      <nav className="navbar">
+        <div className="logo">
+          <Link className="" to="#">
+            RecipiX
           </Link>
-          <button
-            className="navbar-toggler"
-            type="button"
-            data-bs-toggle="collapse"
-            data-bs-target="#navbarSupportedContent"
-            aria-controls="navbarSupportedContent"
-            aria-expanded="false"
-            aria-label="Toggle navigation">
-            <span className="navbar-toggler-icon"></span>
-          </button>
-          <div className="collapse navbar-collapse" id="navbarSupportedContent">
-            <ul className="navbar-nav me-auto mb-2 mb-lg-0">
-              <li className="nav-item">
-                <Link className="nav-link active" aria-current="page" to="#">
-                  Home
-                </Link>
-              </li>
-              <li className="nav-item">
-                <Link className="nav-link" to="#">
-                  Link
-                </Link>
-              </li>
-              <li className="nav-item dropdown">
-                <Link
-                  className="nav-link dropdown-toggle"
-                  to="#"
-                  role="button"
-                  data-bs-toggle="dropdown"
-                  aria-expanded="false">
-                  Dropdown
-                </Link>
-                <ul className="dropdown-menu">
-                  <li>
-                    <Link className="dropdown-item" to="#">
-                      Action
-                    </Link>
-                  </li>
-                  <li>
-                    <Link className="dropdown-item" to="#">
-                      Another action
-                    </Link>
-                  </li>
-                  <li>
-                    <hr className="dropdown-divider" />
-                  </li>
-                  <li>
-                    <Link className="dropdown-item" to="#">
-                      Something else here
-                    </Link>
-                  </li>
-                </ul>
-              </li>
-              <li className="nav-item">
-                <Link className="nav-link disabled" aria-disabled="true">
-                  Disabled
-                </Link>
-              </li>
-            </ul>
-            <form className="d-flex" role="search">
-              <input className="form-control me-2" type="search" placeholder="Search" aria-label="Search" />
-              <button className="btn btn-outline-success" type="submit">
-                Search
+        </div>
+        <div className="search-bar">
+          <form class="example" action="action_page.php">
+            <input type="text" placeholder="Search.." name="search" />
+            <button type="submit">
+            <img className="Search-icon" src="./Search-icon.png" alt=""></img>
+            </button>
+          </form>
+          <div>
+            {isAuthenticated() && (
+              <button
+                className="btn btn-warning"
+                onClick={() => signout(() => navigate("/login"))}
+              >
+                Signout
               </button>
-            </form>
-            <div>
-              {isAuthenticated() && (
-                <button className="btn btn-warning" onClick={() => signout(() => navigate("/login"))}>
-                  Signout
-                </button>
-              )}
-              {!isAuthenticated() && (
-                <Link to="/login">
-                  <i className="bi bi-person-circle p-5"></i>
-                </Link>
-              )}
-            </div>
+            )}
+            {!isAuthenticated() && (
+              <Link to="/login">
+                <i className=""></i>
+              </Link>
+            )}
           </div>
+        </div>
+        <div className="nav-pages">
+          <ul className="nav-links">
+            <li className="links">
+              <Link className="" aria-current="page" to="#">
+                Home
+              </Link>
+            </li>
+            <li className="links">
+              <Link className="" to="#">
+                Explore
+              </Link>
+            </li>
+            <li className="links">
+              <Link className="" aria-disabled="true">
+                Help
+              </Link>
+            </li>
+          </ul>
+        </div>
+        <div className="User">
+          <img src="./user-icon.png" alt="image"></img>
         </div>
       </nav>
     </>
