35f53a2f2fc530f4e559d628f0d607114067755e
Rahul Maharjan <rahulmaharjan252@gmail.com>
2024-01-22 23:45:59 +0545

    Changes in Footer

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -1,146 +1,82 @@
 import React from "react";
+import "./Footer.css";
 import { Link } from "react-router-dom";
+import recipix from "../images/Recipix.png";
+// import search from "../images/Search-icon.png";
 
-const Footer = () => {
-  return (
-    <>
-      <div className="container">
-        <footer className="pt-5">
-          <div className="row">
-            <div className="col-6 col-md-2 mb-3">
-              <h5>Section</h5>
-              <ul className="nav flex-column">
-                <li className="nav-item mb-2">
-                  <Link to="#" className="nav-link p-0 text-body-secondary">
-                    Home
-                  </Link>
-                </li>
-                <li className="nav-item mb-2">
-                  <Link to="#" className="nav-link p-0 text-body-secondary">
-                    Features
-                  </Link>
-                </li>
-                <li className="nav-item mb-2">
-                  <Link to="#" className="nav-link p-0 text-body-secondary">
-                    Pricing
-                  </Link>
-                </li>
-                <li className="nav-item mb-2">
-                  <Link to="#" className="nav-link p-0 text-body-secondary">
-                    FAQs
-                  </Link>
-                </li>
-                <li className="nav-item mb-2">
-                  <Link to="#" className="nav-link p-0 text-body-secondary">
-                    About
-                  </Link>
-                </li>
-              </ul>
-            </div>
-
-            <div className="col-6 col-md-2 mb-3">
-              <h5>Section</h5>
-              <ul className="nav flex-column">
-                <li className="nav-item mb-2">
-                  <Link to="#" className="nav-link p-0 text-body-secondary">
-                    Home
-                  </Link>
-                </li>
-                <li className="nav-item mb-2">
-                  <Link to="#" className="nav-link p-0 text-body-secondary">
-                    Features
-                  </Link>
-                </li>
-                <li className="nav-item mb-2">
-                  <Link to="#" className="nav-link p-0 text-body-secondary">
-                    Pricing
-                  </Link>
-                </li>
-                <li className="nav-item mb-2">
-                  <Link to="#" className="nav-link p-0 text-body-secondary">
-                    FAQs
-                  </Link>
-                </li>
-                <li className="nav-item mb-2">
-                  <Link to="#" className="nav-link p-0 text-body-secondary">
-                    About
-                  </Link>
-                </li>
-              </ul>
-            </div>
-
-            <div className="col-6 col-md-2 mb-3">
-              <h5>Section</h5>
-              <ul className="nav flex-column">
-                <li className="nav-item mb-2">
-                  <Link to="#" className="nav-link p-0 text-body-secondary">
-                    Home
-                  </Link>
-                </li>
-                <li className="nav-item mb-2">
-                  <Link to="#" className="nav-link p-0 text-body-secondary">
-                    Features
-                  </Link>
-                </li>
-                <li className="nav-item mb-2">
-                  <Link to="#" className="nav-link p-0 text-body-secondary">
-                    Pricing
-                  </Link>
-                </li>
-                <li className="nav-item mb-2">
-                  <Link to="#" className="nav-link p-0 text-body-secondary">
-                    FAQs
-                  </Link>
-                </li>
-                <li className="nav-item mb-2">
-                  <Link to="#" className="nav-link p-0 text-body-secondary">
-                    About
-                  </Link>
-                </li>
-              </ul>
-            </div>
-
-            <div className="col-md-5 offset-md-1 mb-3">
-              <form>
-                <h5>Subscribe to our newsletter</h5>
-                <p>Monthly digest of what's new and exciting from us.</p>
-                <div className="d-flex flex-column flex-sm-row w-100 gap-2">
-                  <label for="newsletter1" className="visually-hidden">
-                    Email address
-                  </label>
-                  <input id="newsletter1" type="text" className="form-control" placeholder="Email address" />
-                  <button className="btn btn-primary" type="button">
-                    Subscribe
-                  </button>
-                </div>
-              </form>
-            </div>
-          </div>
-
-          <div className="d-flex flex-column flex-sm-row justify-content-between pt-4 mt-4 border-top">
-            <p>&copy; 2023 Company, Inc. All rights reserved.</p>
-            <ul className="list-unstyled d-flex">
-              <li className="ms-3">
-                <Link className="link-body-emphasis" to="#">
-                  <svg className="bi" width="24" height="24"></svg>
-                </Link>
-              </li>
-              <li className="ms-3">
-                <Link className="link-body-emphasis" to="#">
-                  <svg className="bi" width="24" height="24"></svg>
-                </Link>
-              </li>
-              <li className="ms-3">
-                <Link className="link-body-emphasis" to="#">
-                  <svg className="bi" width="24" height="24"></svg>
-                </Link>
-              </li>
-            </ul>
-          </div>
-        </footer>
+const Footer = () => (
+  <footer className="footer">
+    <div className="footer-container">
+      <div className="footer-column">
+        <div className="logo-footer">
+          <Link className="" to="/">
+            <img className="img-logo" src={recipix} alt=""></img>
+            RecipiX
+          </Link>
+        </div>
+        <ul>
+          <li>
+            <a href="/about">About</a>
+          </li>
+          <li>
+            <a href="/blog">Blog</a>
+          </li>
+          <li>
+            <a href="/recipes">Recipe Index</a>
+          </li>
+          <li>
+            <a href="/sponsored-content">Sponsored Content</a>
+          </li>
+          <li>
+            <a href="/media-mentions">Media Mentions</a>
+          </li>
+          <li>
+            <a href="/contact">Contact</a>
+          </li>
+        </ul>
       </div>
-    </>
-  );
-};
+      <div className="footer-column">
+        <h3>FOOD & RECIPES</h3>
+        <ul>
+          <li>
+            <a href="/sugar-free-january">Sugar Free</a>
+          </li>
+          <li>
+            <a href="/quick-and-easy-recipes">Quick and Easy Recipes</a>
+          </li>
+          <li>
+            <a href="/instant-pot-recipes">Instant Pot Recipes</a>
+          </li>
+          <li>
+            <a href="/pasta-recipes">Pasta Recipes</a>
+          </li>
+          <li>
+            <a href="/vegan-recipes">Vegan Recipes</a>
+          </li>
+          <li>
+            <a href="/soup-recipes">Soup Recipes</a>
+          </li>
+          <li>
+            <a href="/meal-prep-recipes">Meal Prep Recipes</a>
+          </li>
+        </ul>
+      </div>
+      <div className="footer-column">
+        <h3>SIGN UP FOR EMAIL UPDATES</h3>
+        <p>Get free weekly recipes, meal plans, and tips!</p>
+        <form>
+          <input type="text" placeholder="First Name" />
+          <input type="email" placeholder="Email" />
+          <button type="submit">GO</button>
+        </form>
+      </div>
+    </div>
+    <div className="footer-bottom">
+      <p>&copy; 2024 RecipiX. All Rights Reserved.</p>
+      <a href="/privacy-policy">Privacy Policy</a>
+      <a href="/terms">Terms</a>
+    </div>
+  </footer>
+);
 
 export default Footer;
